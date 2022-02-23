748d9674df9f0c3d36b31fde14b330d7e91bcc83
Ormec <98923876+Ormec@users.noreply.github.com>
2022-02-23 12:59:02 -0600

    Update ejercicio_01.py

--- ejercicio_01.py
+++ ejercicio_01.py
@@ -1,34 +1,27 @@
 def suma(a, b):
   print(a + b)
-  
-suma(10, 5)
+  suma(10,5)
 
 def resta(a, b):
-  print(a - b)
-  
-resta(10, 5)
+  print(a - b)  
+  resta(10,5)
 
 def multiplicacion(a, b):
   print(a * b)
-  
-multiplicacion(10, 5)
+  multiplicacion(10,5)
 
 def division(a, b):
   print(a / b)
-  
-division(10, 5)
+  division(10,5)
 
 def modulo(a, b):
   print(a % b)
-  
-modulo(10, 7)
+  modulo(10,7)
   
 def potencia(a, b):
   print(a ** b)
-  
-potencia(10, 5)
+  potencia(10,5)
 
 def division_entera(a, b):
   print(a // b)
-  
-division_entera(10, 7)
+  division_entera(10,7)
